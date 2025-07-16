2950af67a35e752cb3e80fbfa54fc00b4cedbb98
YashMachhewar <yashmachhewar21@gmail.com>
2025-07-16 08:56:54 +0000

    variables and datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer Version
+
+// alert( 3 + 3) //NOt work in nodeJS work in Browser or Browser Console
+
+//console.log(3+3); console.log("Yash"); //code readiblity should be high
+
+let name = "Yash"  //String Datatype
+let age = 19 //Int datatype
+let isLoggedIN = false //boolean datatype
+let State = null; //NuLL Datatype  (empty)
+let hobby; //undefined Datatype
+
+console.log(typeof "yash");
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
+
+
---
+++ 01_basics/variables.js
@@ -0,0 +1,20 @@
+const accountId = 2128
+let accountEmail = "yashraj@gmail.com"
+var accountPassword = "12345"
+accountCity = "Nanded"
+let accountState
+
+// accountId = 21 //Not allowed
+accountEmail = "dfohfio@gnk.com"
+accountPassword = "457"
+accountCity = "Kinwat"
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState]);
+
+
+
