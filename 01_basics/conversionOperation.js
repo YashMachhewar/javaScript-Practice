07a02b8c4201ac20d49a5c4c40fcd6833ff3700c
YashMachhewar <yashmachhewar21@gmail.com>
2025-07-16 09:25:45 +0000

    conversion of datatypes

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,41 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(score);
+
+console.log(typeof valueInNumber);
+console.log( valueInNumber);
+//========================================================================
+let marks = "232fojoi"
+console.log(typeof score);
+console.log(typeof (score));
+
+let changeInNUmber = Number(score)
+console.log(score);
+
+console.log(typeof changeInNUmber);
+console.log( changeInNUmber);
+
+/*
+33 => output 33
+"33ab" => NaN (Not a Number)
+true => 1 ; false =>0
+*/
+
+//let isLoggedIN = 1; //=>true
+//let isLoggedIN = ""; //=>false
+//let isLoggedIN = "Yash"; //=>ture
+let isLoggedIN = 23;
+let booleanisLoggedIn = Boolean(isLoggedIN);
+console.log(booleanisLoggedIn);
+
+//=======================================================
+//to String
+
+let someNumber = 23;
+let numtoString = String(Number);
+console.log(numtoString);
+console.log(typeof numtoString);
